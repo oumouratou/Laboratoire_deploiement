93913340e3bd0fc7907e093fda1098092a0ab358
oumouratou <oumouratoubarry601@gmail.com>
2026-04-06 20:01:35 +0100

    Responsive mobile/tablette: layouts et navigation

--- vue-js/vite.config.ts
+++ vue-js/vite.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vite.dev/config/
 export default defineConfig({
-  
-base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/Gestion_labo/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
